366ec227b369e45d73c82e572b5152c5a62d7360
GarryMcKee <gmk0232@gmail.com>
2018-02-10 17:34:14 +0000

    Add check for Google Play Api Availability

--- app/src/main/java/com/mellobit/garrymckee/locatr/LocatrActivity.java
+++ app/src/main/java/com/mellobit/garrymckee/locatr/LocatrActivity.java
@@ -1,10 +1,17 @@
 package com.mellobit.garrymckee.locatr;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class LocatrActivity extends SingleFragmentActivity {
+
+    private static final int REQUEST_ERROR = 0;
 
     @Override
     protected Fragment createFragment() {
@@ -15,5 +22,25 @@ public class LocatrActivity extends SingleFragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+        if(errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = apiAvailability
+                    .getErrorDialog(this, errorCode, REQUEST_ERROR, new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialogInterface) {
+                            finish();
+                        }
+                    });
+
+            errorDialog.show();
+        }
     }
 }
